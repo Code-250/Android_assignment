8af678ab03d68b1b02780c13e917fdb7706cb826
Code-250 <richmunye@gmail.com>
2021-11-26 10:13:15 +0200

    added colors and toolbar together with starting a list view

--- app/src/main/java/com/example/android_assignment/MainActivity.java
+++ app/src/main/java/com/example/android_assignment/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.android_assignment;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toolbar;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,6 +20,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        change fonts for the toolbar
+
+        TextView textView = findViewById(R.id.text_toolbar);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.dance_script_bold);
+        textView.setTypeface(typeface);
 
 //        initialize and assign variables
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
